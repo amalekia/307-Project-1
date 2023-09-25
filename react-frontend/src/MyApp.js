2d8e920767bb3b2e756ed25f2b1bb272848490ba
Adrick Malekian <adrickmalekian@Adricks-iMac.local>
2023-09-24 17:16:19 -0700

    comitting to git

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -2,7 +2,7 @@ import React, {useState} from 'react';
 import Table from "./Table";
 
 function MyApp() {
-  const [characters, setCharacters] = [
+  const [characters, setCharacters] = useState([
     {
       name: "Charlie",
       job: "Janitor",
@@ -19,7 +19,14 @@ function MyApp() {
       name: "Dennis",
       job: "Bartender",
     },
-  ];
+  ]);
+
+  function removeOneCharacter (index) {
+      const updated = characters.filter((character, i) => {
+        return i != index
+      });
+    setCharacters(updated);
+  }
 
     return (
         <div className="container">
