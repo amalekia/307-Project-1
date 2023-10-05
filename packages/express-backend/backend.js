b3f1eda5f394d5ea71dd4218493399c4923b19f7
Adrick Malekian <adrickmalekian@Adricks-iMac.local>
2023-10-05 11:52:36 -0700

    done with everything except part 6

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -4,83 +4,77 @@ const app = express();
 const port = 8000;
 
 const users = {
-  users_list : [
-    { 
-       id : 'xyz789',
-       name : 'Charlie',
-       job: 'Janitor',
+  users_list: [
+    {
+      id: "xyz789",
+      name: "Charlie",
+      job: "Janitor",
     },
     {
-       id : 'abc123', 
-       name: 'Mac',
-       job: 'Bouncer',
+      id: "abc123",
+      name: "Mac",
+      job: "Bouncer",
     },
     {
-       id : 'ppp222', 
-       name: 'Mac',
-       job: 'Professor',
-    }, 
-    {
-       id: 'yat999', 
-       name: 'Dee',
-       job: 'Aspring actress',
+      id: "ppp222",
+      name: "Mac",
+      job: "Professor",
     },
     {
-       id: 'zap555', 
-       name: 'Dennis',
-       job: 'Bartender',
-    }
- ]
-}
-
+      id: "yat999",
+      name: "Dee",
+      job: "Aspring actress",
+    },
+    {
+      id: "zap555",
+      name: "Dennis",
+      job: "Bartender",
+    },
+  ],
+};
 
 app.use(express.json());
 
 //helper functions
 const findUserByName = (name) => {
-  return users['users_list']
-        .filter( (user) => user['name'] === name);
-}
+  return users["users_list"].filter((user) => user["name"] === name);
+};
 
 const findUserById = (id) =>
-    users['users_list']
-          .find( (user) => user['id'] === id);
+  users["users_list"].find((user) => user["id"] === id);
 
-
-const addUser = (user) =>  {
-  users['users_list'].push(user);
+const addUser = (user) => {
+  users["users_list"].push(user);
   return user;
-}
+};
 
 //GET endpoints
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get('/users', (req, res) => {
+app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined){
+  if (name != undefined) {
     let result = findUserByName(name);
-    result = {users_list: result};
+    result = { users_list: result };
     res.send(result);
-  }
-  else {
+  } else {
     res.send(users);
   }
 });
 
-app.get('/users/:id', (req, res) => {
-  const id = req.params['id'];
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"];
   let result = findUserById(id);
   if (result === undefined) {
-    res.status(404).send('Resource not found.');
-  }
-  else {
+    res.status(404).send("Resource not found.");
+  } else {
     res.send(result);
   }
 });
 
-app.post('/users', (req, res) => {
+app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
@@ -90,6 +84,3 @@ app.post('/users', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
-
