93a1a02983b9032cb996e92d9cf5aa9c668d8222
Adrick Malekian <adrickmalekian@Adricks-iMac.local>
2023-10-07 17:14:07 -0700

    switching branceh again

--- packages/react-frontend/src/MyApp.js
+++ packages/react-frontend/src/MyApp.js
@@ -1,14 +1,23 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter (index) {
-      const updated = characters.filter((character, i) => {
-        return i !== index
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
       });
+  }, []);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
     setCharacters(updated);
   }
 
@@ -17,7 +26,7 @@ function MyApp() {
       .then(() => setCharacters([...characters, person]))
       .catch((error) => {
         console.log(error);
-      })
+      });
   }
 
   function fetchUsers() {
@@ -37,21 +46,12 @@ function MyApp() {
     return promise;
   }
 
-
-  useEffect(() => {
-    fetchUsers()
-              .then((res) => res.json())
-              .then((json) => setCharacters(json["users_list"]))
-              .catch((error) => { console.log(error); });
-  }, []);
-
-    return (
-        <div className="container">
-          <Table characterData={characters}
-                removeCharacter = {removeOneCharacter} />
-          <Form handleSubmit={updateList}/>
-        </div>
-    );
+  return (
+    <div className="container">
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
+      <Form handleSubmit={updateList} />
+    </div>
+  );
 }
 
 export default MyApp;
